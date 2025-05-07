4c84b1131063a8c8d5451029a36eb341d7fb5f41
pcdef <pc.defraga@gmail.com>
2025-05-07 15:36:03 -0300

    Criação do helper

---
+++ Frontend/RestaurantDashboard/src/utils/helper.ts
@@ -0,0 +1,40 @@
+
+export async function apiFetch(endpoint: string, options: RequestInit = {}) {
+
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+
+  const token = localStorage.getItem("token");
+
+
+  const headers = {
+    "Content-Type": "application/json",
+
+    ...(token && { Authorization: `Bearer ${token}` }),
+    ...options.headers,
+  };
+
+  try {
+
+    const response = await fetch(`${apiUrl}${endpoint}`, {
+      ...options,
+      headers,
+    });
+
+
+    if (!response.ok) {
+
+      const errorData = await response.json();
+
+      throw new Error(errorData.message || "Erro na requisição");
+    }
+
+
+    return await response.json();
+
+  } catch (error: any) {
+
+    console.error("Erro ao fazer requisição:", error);
+    throw new Error(error.message || "Erro desconhecido ao se comunicar com a API");
+  }
+}
