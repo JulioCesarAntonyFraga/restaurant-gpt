16620b42ed28e4d9f4709f2fbf6c509ed5d45086
si-jufr <jufr@siteimprove.com>
2025-05-09 19:39:09 +0100

    Removido autenticação para este endpoint

--- Backend/RetrieveMenuItems/__init__.py
+++ Backend/RetrieveMenuItems/__init__.py
@@ -4,10 +4,6 @@ from utils.storage import get_menu
 from utils.auth import verify_token
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    user = verify_token(req)
-    if not user:
-        return func.HttpResponse("Unauthorized", status_code=401)
-    
     menu = get_menu()
 
     return func.HttpResponse(json.dumps(menu), mimetype="application/json", status_code=200)
