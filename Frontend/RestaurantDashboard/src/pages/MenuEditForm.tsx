158be67c70171a4274ae18fa0f3165244a3773a2
Pcfraga <158301052+Pcfraga@users.noreply.github.com>
2025-05-14 16:57:30 -0300

    Update MenuEditForm.tsx

--- Frontend/RestaurantDashboard/src/pages/MenuEditForm.tsx
+++ Frontend/RestaurantDashboard/src/pages/MenuEditForm.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { apiFetch } from "../utils/helper";
+import { apiFetch } from "../utils/apiHelper";
 
 type MenuItem = {
   name: string;
