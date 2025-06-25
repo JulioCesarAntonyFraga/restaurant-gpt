a322435fd918e4ebadcb12f336ceddbb76b347a2
pcdef <pc.defraga@gmail.com>
2025-06-25 14:31:50 -0300

    inserindo infos de endereco e correcao dos nomes no card

--- Frontend/RestaurantDashboard/src/components/OrderInfo.tsx
+++ Frontend/RestaurantDashboard/src/components/OrderInfo.tsx
@@ -1,13 +1,20 @@
 import { formatTimestamp } from "../utils/orderUtils";
 
 export interface OrderInfoProps {
+    name?: string;
     phone: string;
     isDelivery: boolean;
     orderedAt: number;
     status: number;
+    cep?: string;
+    rua?: string;
+    numero?: string;
+    bairro?: string;
+    cidade?: string;
+
   }
   
-  const OrderInfo = ({ phone, isDelivery, orderedAt, status }: OrderInfoProps) => {
+  const OrderInfo = ({ name, phone, isDelivery, orderedAt, status, cep, rua, numero, bairro, cidade }: OrderInfoProps) => {
     const statusOptions = [
         { value: 0, label: "Pendente" },
         { value: 1, label: "Aceito" },
@@ -19,13 +26,24 @@ export interface OrderInfoProps {
 
     return (
       <div>
-        <p><strong>Phone:</strong> {phone}</p>
-        <p><strong>Type:</strong> {isDelivery ? "Delivery" : "Take Away"}</p>
-        <p><strong>Time:</strong> {formatTimestamp(orderedAt)}</p>
-        <p><strong>Status:</strong> {statusOptions.find(opt => opt.value === status)?.label || "Desconhecido"}</p>
-      </div>
-    );
-  };
+        <p><strong>Nome:</strong> {name}</p>
+        <p><strong>Telefone:</strong> {phone}</p>
+      <p><strong>Tipo:</strong> {isDelivery ? "Delivery" : "Take Away"}</p>
+      <p><strong>Horário:</strong> {formatTimestamp(orderedAt)}</p>
+      <p><strong>Situação:</strong> {statusOptions.find(opt => opt.value === status)?.label || "Desconhecido"}</p>
+
+      {isDelivery && (
+        <div className="mt-2 text-sm text-gray-700">
+          <p><strong>CEP:</strong> {cep}</p>
+          <p><strong>Endereço:</strong> {rua}, {numero}</p>
+          <p><strong>Bairro:</strong> {bairro}</p>
+          <p><strong>Cidade:</strong> {cidade}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
   
   export default OrderInfo;
   
